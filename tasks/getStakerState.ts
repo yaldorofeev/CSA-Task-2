75dacbfb2dd6fccfa728b948620c01822881116d
yaldorofeev <yaldorofeev@gmail.com>
2022-05-23 12:50:30 +0300

    some fix

--- tasks/getStakerState.ts
+++ tasks/getStakerState.ts
@@ -6,7 +6,7 @@ task("getStakerState", "Get info about stakes")
   .addParam("requesting", "ID of accaunt in array in .env")
   .setAction(async (args, hre) => {
   const accounts = await hre.ethers.getSigners();
-  const contract = await hre.ethers.getContractAt("ISuperStaking",
+  const contract = await hre.ethers.getContractAt("SuperStaking",
   process.env.CONTRACT_ACCAUNT!, accounts[args.requesting]);
   const tx = await contract.getStakerState();
   tx.wait();
